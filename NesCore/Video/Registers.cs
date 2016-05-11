ef62dd5f5cb630856dcab5183c071ab75f4b1b54
colinvella <colinvella@gmail.com>
2016-05-11 17:44:21 +0200

    Started implementation of PPU.

---
+++ NesCore/Video/Registers.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NesCore.Video
+{
+    public class Registers
+    {
+        /// <summary>
+        /// PPUCTRL $2000
+        /// </summary>
+        public byte Control { get; set; }
+
+        /// <summary>
+        /// PPUMASK $2001
+        /// </summary>
+        public byte Mask { get; set; }
+
+        /// <summary>
+        /// PPUSTATUS $2002
+        /// </summary>
+        public byte Status { get; set; }
+
+        /// <summary>
+        /// OAMADDR $2003
+        /// </summary>
+        public byte ObjectAttributeMemoryAddress { get; set; }
+
+        /// <summary>
+        /// OAMDATA $2004
+        /// </summary>
+        public byte ObjectAttributeMemoryData { get; set; }
+
+        /// <summary>
+        /// PPUSCROLL $2005
+        /// </summary>
+        public byte Scroll { get; set; } // TODO: write two times X, Y
+
+        /// <summary>
+        /// PPUADDR $2006
+        /// </summary>
+        public byte Address { get; set; } // TODO: write two times Hi, Lo
+
+        /// <summary>
+        /// PPUDATA $2007
+        /// </summary>
+        public byte Data { get; set; }
+
+        /// <summary>
+        /// OAMDMA $4014
+        /// </summary>
+        public byte DirectMemoryAccess { get; set; }
+
+    }
+}
---
+++ NesCore/Video/RicohRP2C0X.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NesCore.Video
+{
+    public class RicohRP2C0X
+    {
+        public RicohRP2C0X()
+        {
+            Registers = new Registers();
+        }
+
+        public Registers Registers { get; private set; }
+
+
+    }
+}

1d45f40d743b1420b11306376864b37d79a7ae3c
colinvella <colinvella@gmail.com>
2016-05-11 13:36:10 +0200

    Started connection and configuration of memory with processor.

--- NesCore/Console.cs
+++ NesCore/Console.cs
@@ -14,10 +14,36 @@ namespace NesCore
         public Console()
         {
             Processor = new Mos6502();
-            //Memory = new Memory();
+
+            Memory = CreateConfiguredMemoryMap();
+
+            // connect processor to memory
+            Processor.ReadByte = (ushort address) => { return Memory[address]; };
+            Processor.WriteByte = (ushort address, byte value) => { Memory[address] = value; };
         }
 
         public Mos6502 Processor { get; private set; }
-        //public Memory Memory { get; private set; }
+        public IMemoryMap Memory { get; private set; }
+
+        private IMemoryMap CreateConfiguredMemoryMap()
+        {
+            ConfigurableMemoryMap memory = new ConfigurableMemoryMap();
+
+            // work ram mirrored 4 times (4 x $800 segments)
+            memory.ConfigureAddressMirroring(0x000, 0x0800, 0x2000);
+
+            // $2000-$2007 PPU registers (TODO)
+            memory.ConfigureMemoryRead(0x2000, (address) => { return 0; } );
+            memory.ConfigureMemoryWrite(0x2000, (address, value) => { } );
+
+            // 8 PPU registers mirrered all the way up to $3FFF
+            memory.ConfigureAddressMirroring(0x2000, 0x0008, 0x2000);
+
+            // $4000-$401F APU and I/O reSgisters (TODO)
+
+            // $4020-$FFFF Cartridge space: PRG ROM, PRG RAM, and mapper registers (TODO)
+
+            return memory;
+        }
     }
 }

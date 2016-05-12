1aa2fab0b8f927f3dd0341990611e28defbd6b87
colinvella <colinvella@gmail.com>
2016-05-12 15:55:31 +0200

    Added Palette and Colour classes.

---
+++ NesCore/Video/Colour.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NesCore.Video
+{
+    public class Colour
+    {
+        public Colour(byte red, byte green, byte blue)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+        }
+
+        public Colour(ulong colourValue)
+        {
+            Red = (byte)(colourValue >> 16);
+            Green = (byte)(colourValue >> 8);
+            Blue = (byte)colourValue;
+        }
+
+        public byte Red { get; private set; }
+        public byte Green { get; private set; }
+        public byte Blue { get; private set; }
+    }
+}

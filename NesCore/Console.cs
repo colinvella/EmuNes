afb9689205b19963b9a7742cabaebea6f2a41914
colinvella <colinvella@gmail.com>
2016-05-18 14:27:46 +0200

    Implemented Controller Interface, Joypad and console methods wire controllers to memory map.

--- NesCore/Console.cs
+++ NesCore/Console.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using NesCore.Processor;
 using NesCore.Video;
 using NesCore.Memory;
+using NesCore.Input;
 
 namespace NesCore
 {
@@ -25,9 +26,19 @@ namespace NesCore
 
         public Mos6502 Processor { get; private set; }
         public RicohRP2C0X Video { get; private set; }
-        public IMemoryMap Memory { get; private set; }
+        public ConfigurableMemoryMap Memory { get; private set; }
 
-        private IMemoryMap CreateConfiguredMemoryMap()
+        public void ConnectControllerOne(Controller controller)
+        {
+            ConnectController(0x4016, controller);
+        }
+
+        public void ConnectControllerTwo(Controller controller)
+        {
+            ConnectController(0x4017, controller);
+        }
+
+        private ConfigurableMemoryMap CreateConfiguredMemoryMap()
         {
             ConfigurableMemoryMap memory = new ConfigurableMemoryMap();
 
@@ -37,44 +48,44 @@ namespace NesCore
             // $2000-$2007 PPU registers
 
             // $2000 PPUCTRL
-            memory.ConfigureMemoryRead(0x2000, (address) => { return Video.Control; });
-            memory.ConfigureMemoryWrite(0x2000, (address, value) => { Video.Control = value; } );
+            memory.ConfigureMemoryRead(0x2000, (address) => Video.Control);
+            memory.ConfigureMemoryWrite(0x2000, (address, value) => Video.Control = value);
 
             // $2001 PPUMASK
-            memory.ConfigureMemoryRead(0x2001, (address) => { return Video.Mask; });
-            memory.ConfigureMemoryWrite(0x2001, (address, value) => { Video.Mask = value; });
+            memory.ConfigureMemoryRead(0x2001, (address) => Video.Mask);
+            memory.ConfigureMemoryWrite(0x2001, (address, value) => Video.Mask = value);
 
             // $2002 PPUSTATUS
-            memory.ConfigureMemoryRead(0x2002, (address) => { return Video.Status; });
-            memory.ConfigureMemoryWrite(0x2002, (address, value) => { Video.Status = value; });
+            memory.ConfigureMemoryRead(0x2002, (address) => Video.Status);
+            memory.ConfigureMemoryWrite(0x2002, (address, value) => Video.Status = value);
 
             // $2003 OAMADDR
-            memory.ConfigureMemoryRead(0x2003, (address) => { return Video.ObjectAttributeMemoryAddress; });
-            memory.ConfigureMemoryWrite(0x2003, (address, value) => { Video.ObjectAttributeMemoryAddress = value; });
+            memory.ConfigureMemoryRead(0x2003, (address) => Video.ObjectAttributeMemoryAddress);
+            memory.ConfigureMemoryWrite(0x2003, (address, value) => Video.ObjectAttributeMemoryAddress = value);
 
             // $2004 OAMDATA
-            memory.ConfigureMemoryRead(0x2004, (address) => { return Video.ObjectAttributeMemoryData; });
-            memory.ConfigureMemoryWrite(0x2004, (address, value) => { Video.ObjectAttributeMemoryData = value; });
+            memory.ConfigureMemoryRead(0x2004, (address) => Video.ObjectAttributeMemoryData);
+            memory.ConfigureMemoryWrite(0x2004, (address, value) => Video.ObjectAttributeMemoryData = value);
 
             // $2005 PPUSCROLL
-            memory.ConfigureMemoryRead(0x2005, (address) => { return Video.Scroll; });
+            memory.ConfigureMemoryRead(0x2005, (address) => Video.Scroll);
             memory.ConfigureMemoryWrite(0x2005, (address, value) => { Video.Scroll = value; });
 
             // $2006 PPUADDR
-            memory.ConfigureMemoryRead(0x2006, (address) => { return Video.Address; });
-            memory.ConfigureMemoryWrite(0x2006, (address, value) => { Video.Address = value; });
+            memory.ConfigureMemoryRead(0x2006, (address) => Video.Address);
+            memory.ConfigureMemoryWrite(0x2006, (address, value) => Video.Address = value);
 
             // $2007 PPUDATA
-            memory.ConfigureMemoryRead(0x2007, (address) => { return Video.Data; });
-            memory.ConfigureMemoryWrite(0x2007, (address, value) => { Video.Data = value; });
+            memory.ConfigureMemoryRead(0x2007, (address) => Video.Data);
+            memory.ConfigureMemoryWrite(0x2007, (address, value) => Video.Data = value);
 
             // 8 PPU registers mirrered all the way up to $3FFF
             memory.ConfigureAddressMirroring(0x2000, 0x0008, 0x2000);
 
-            // $4000-$401F APU and I/O reSgisters (TODO)
+            // $4000-$401F APU and I/O registers (TODO)
 
             // $4014 OAMDMA
-            memory.ConfigureMemoryRead(0x4014, (address) => { return Video.ObjectAttributeDirectMemoryAccess; });
+            memory.ConfigureMemoryRead(0x4014, (address) => Video.ObjectAttributeDirectMemoryAccess);
             memory.ConfigureMemoryWrite(0x4014, (address, value) =>
             {
                 Video.ObjectAttributeDirectMemoryAccess = value;
@@ -88,6 +99,14 @@ namespace NesCore
             // $4020-$FFFF Cartridge space: PRG ROM, PRG RAM, and mapper registers (TODO)
 
             return memory;
+        }
+
+        private void ConnectController(ushort portAddress, Controller controller)
+        {
+            if (portAddress != 0x4016 && portAddress != 0x4017)
+                throw new ArgumentOutOfRangeException("portAddress", portAddress, "Allowed port addresses are $4016 ans $4017");
+            Memory.ConfigureMemoryRead(portAddress, (address) => controller.Port);
+            Memory.ConfigureMemoryWrite(portAddress, (address, value) => controller.Port = value);
         }
     }
 }
---
+++ NesCore/Input/Joypad.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NesCore.Input
+{
+    public delegate bool ButtonPressed();
+
+    public class Joypad: Controller
+    {
+        public Joypad()
+        {
+            buttonPressed = new bool[8];
+            buttonIndex = 0x00;
+            A = B = Start = Select = Up = Down = Left = Right = () => false;
+        }
+
+        public ButtonPressed A { get; set; }
+        public ButtonPressed B { get; set; }
+        public ButtonPressed Start { get; set; }
+        public ButtonPressed Select { get; set; }
+        public ButtonPressed Up { get; set; }
+        public ButtonPressed Down { get; set; }
+        public ButtonPressed Left { get; set; }
+        public ButtonPressed Right { get; set; }
+
+        public byte Port
+        {
+            get
+            {
+                byte value = 0x00;
+
+                if (buttonIndex < 8 && buttonPressed[buttonIndex])
+                    value = 0x01;
+                ++buttonIndex;
+
+                if ((strobe & 0x01) == 0x01)
+                    buttonIndex = 0x00;
+
+                return value;
+            }
+            set
+            {
+                strobe = value;
+                if ((strobe & 0x01) == 0x01)
+                    buttonIndex = 0x00;
+            }
+        }
+
+        public void Step()
+        {                              
+            buttonPressed[0] = A();
+            buttonPressed[1] = B();
+            buttonPressed[2] = Start();
+            buttonPressed[3] = Select();
+            buttonPressed[4] = Up();
+            buttonPressed[5] = Down();
+            buttonPressed[6] = Left();
+            buttonPressed[7] = Right();
+        }
+
+
+        private bool[] buttonPressed;
+        private byte buttonIndex;
+        private byte strobe;
+    }
+}

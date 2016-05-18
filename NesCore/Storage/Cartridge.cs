213302a03521a65738f681c19dabe59055032ea6
colinvella <colinvella@gmail.com>
2016-05-18 17:35:54 +0200

    Started cartrige code.

---
+++ NesCore/Storage/Cartridge.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NesCore.Storage
+{
+    public class Cartridge
+    {
+        public IReadOnlyList<byte> ProgramBanks
+        {
+            get { return programBanks; }
+            private set
+            {
+                programBanks = value;
+            }
+        }
+
+        private IReadOnlyList<byte> programBanks;
+    }
+}

351f52eecfc43a5e357f66968addaea0b9d53d09
colinvella <colinvella@gmail.com>
2016-05-19 15:38:24 +0200

    Implemented Cartridge class.

--- NesCore/Storage/Cartridge.cs
+++ NesCore/Storage/Cartridge.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,15 +9,62 @@ namespace NesCore.Storage
 {
     public class Cartridge
     {
-        public IReadOnlyList<byte> ProgramBanks
+        public Cartridge(BinaryReader romBinaryReader)
         {
-            get { return programBanks; }
-            private set
+            SaveRam = new byte[0x2000];
+
+            uint magicNumber = romBinaryReader.ReadUInt32();
+
+            if (magicNumber != InesMagicNumber)
+                throw new InvalidDataException("INES Magic Number mismatch");
+
+            // read header
+            byte programBankCount = romBinaryReader.ReadByte();
+            byte characterBankCount = romBinaryReader.ReadByte();
+            byte controlBits1 = romBinaryReader.ReadByte();
+            byte controlBits2 = romBinaryReader.ReadByte();
+            byte programRamSize = romBinaryReader.ReadByte();
+            romBinaryReader.ReadByte(); // unused byte
+
+            // determine mapper type from control bits
+            int mapperTypeLowerNybble = controlBits1 >> 4;
+            int mapperTypeHigherNybble = controlBits2 >> 4;
+            MapperType = (byte)((mapperTypeHigherNybble << 4) | mapperTypeLowerNybble);
+
+            // determine mirroring mode
+            int mirrorLowBit = controlBits1 & 1;
+            int mirrorHighBit = (controlBits1 >> 3) & 1;
+            MirrorMode = (byte)((mirrorHighBit << 1) | mirrorLowBit);
+
+            // battery-backed RAM
+            BatteryPresent = (controlBits1 & 0x2) != 0;
+
+            // read trainer if present (unused)
+            if ((controlBits1 & 0x04) == 0x04)
             {
-                programBanks = value;
+                byte[] trainer = romBinaryReader.ReadBytes(512);
             }
+
+            // read prg-rom bank(s)
+            byte[] programData = romBinaryReader.ReadBytes(programBankCount * 0x4000);
+            ProgramRom = new List<byte>(programData);
+
+            // read chr-rom bank(s)
+            byte[] characterData = characterBankCount == 0
+                ? new byte[0x2000] // at least one default empty bank if there are none
+                : romBinaryReader.ReadBytes(characterBankCount * 0x2000);
+            CharacterRom = new List<byte>(characterData);
         }
 
-        private IReadOnlyList<byte> programBanks;
+        public IReadOnlyList<byte> ProgramRom { get; private set; }
+        public IReadOnlyList<byte> CharacterRom { get; private set; }
+        public byte[] SaveRam { get; private set; }
+        public byte MapperType { get; private set; }
+        public byte MirrorMode { get; private set; }
+        public bool BatteryPresent { get; private set; }
+
+        private const uint InesMagicNumber = 0x1a53454e;
     }
+
+   
 }

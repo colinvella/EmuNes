8f4b9135c333b1e0d8769d327fe7289041d3bac0
colinvella <colinvella@gmail.com>
2016-05-23 17:37:34 +0200

    Added hardware in console and some configuration.

--- NesCore/Console.cs
+++ NesCore/Console.cs
@@ -15,13 +15,23 @@ namespace NesCore
     {
         public Console()
         {
+            // create main components
             Processor = new Mos6502();
+            Video = new RicohRP2C0X();
+            Memory = new ConfigurableMemoryMap();
 
-            Memory = CreateConfiguredMemoryMap();
+            // configure hardware and connect components
+            ConfigureMemoryMap();
 
-            // connect processor to memory
+            // connect processor it to memory
             Processor.ReadByte = (ushort address) => { return Memory[address]; };
             Processor.WriteByte = (ushort address, byte value) => { Memory[address] = value; };
+
+            // connect video to memory for DMA operations
+            Video.ReadByte = (ushort address) => { return Memory[address]; };
+
+            // connect default first controller
+            ConnectControllerOne(new Joypad());
         }
 
         public Mos6502 Processor { get; private set; }
@@ -38,55 +48,53 @@ namespace NesCore
             ConnectController(0x4017, controller);
         }
 
-        private ConfigurableMemoryMap CreateConfiguredMemoryMap()
+        private void ConfigureMemoryMap()
         {
-            ConfigurableMemoryMap memory = new ConfigurableMemoryMap(MemorySize);
-
             // work ram mirrored 4 times (4 x $800 segments)
-            memory.ConfigureAddressMirroring(0x000, 0x0800, 0x2000);
+            Memory.ConfigureAddressMirroring(0x000, 0x0800, 0x2000);
 
             // $2000-$2007 PPU registers
 
             // $2000 PPUCTRL
-            memory.ConfigureMemoryRead(0x2000, (address) => Video.Control);
-            memory.ConfigureMemoryWrite(0x2000, (address, value) => Video.Control = value);
+            Memory.ConfigureMemoryRead(0x2000, (address) => Video.Control);
+            Memory.ConfigureMemoryWrite(0x2000, (address, value) => Video.Control = value);
 
             // $2001 PPUMASK
-            memory.ConfigureMemoryRead(0x2001, (address) => Video.Mask);
-            memory.ConfigureMemoryWrite(0x2001, (address, value) => Video.Mask = value);
+            Memory.ConfigureMemoryRead(0x2001, (address) => Video.Mask);
+            Memory.ConfigureMemoryWrite(0x2001, (address, value) => Video.Mask = value);
 
             // $2002 PPUSTATUS
-            memory.ConfigureMemoryRead(0x2002, (address) => Video.Status);
-            memory.ConfigureMemoryWrite(0x2002, (address, value) => Video.Status = value);
+            Memory.ConfigureMemoryRead(0x2002, (address) => Video.Status);
+            Memory.ConfigureMemoryWrite(0x2002, (address, value) => Video.Status = value);
 
             // $2003 OAMADDR
-            memory.ConfigureMemoryRead(0x2003, (address) => Video.ObjectAttributeMemoryAddress);
-            memory.ConfigureMemoryWrite(0x2003, (address, value) => Video.ObjectAttributeMemoryAddress = value);
+            Memory.ConfigureMemoryRead(0x2003, (address) => Video.ObjectAttributeMemoryAddress);
+            Memory.ConfigureMemoryWrite(0x2003, (address, value) => Video.ObjectAttributeMemoryAddress = value);
 
             // $2004 OAMDATA
-            memory.ConfigureMemoryRead(0x2004, (address) => Video.ObjectAttributeMemoryData);
-            memory.ConfigureMemoryWrite(0x2004, (address, value) => Video.ObjectAttributeMemoryData = value);
+            Memory.ConfigureMemoryRead(0x2004, (address) => Video.ObjectAttributeMemoryData);
+            Memory.ConfigureMemoryWrite(0x2004, (address, value) => Video.ObjectAttributeMemoryData = value);
 
             // $2005 PPUSCROLL
-            memory.ConfigureMemoryRead(0x2005, (address) => Video.Scroll);
-            memory.ConfigureMemoryWrite(0x2005, (address, value) => { Video.Scroll = value; });
+            Memory.ConfigureMemoryRead(0x2005, (address) => Video.Scroll);
+            Memory.ConfigureMemoryWrite(0x2005, (address, value) => { Video.Scroll = value; });
 
             // $2006 PPUADDR
-            memory.ConfigureMemoryRead(0x2006, (address) => Video.Address);
-            memory.ConfigureMemoryWrite(0x2006, (address, value) => Video.Address = value);
+            Memory.ConfigureMemoryRead(0x2006, (address) => Video.Address);
+            Memory.ConfigureMemoryWrite(0x2006, (address, value) => Video.Address = value);
 
             // $2007 PPUDATA
-            memory.ConfigureMemoryRead(0x2007, (address) => Video.Data);
-            memory.ConfigureMemoryWrite(0x2007, (address, value) => Video.Data = value);
+            Memory.ConfigureMemoryRead(0x2007, (address) => Video.Data);
+            Memory.ConfigureMemoryWrite(0x2007, (address, value) => Video.Data = value);
 
             // 8 PPU registers mirrered all the way up to $3FFF
-            memory.ConfigureAddressMirroring(0x2000, 0x0008, 0x2000);
+            Memory.ConfigureAddressMirroring(0x2000, 0x0008, 0x2000);
 
             // $4000-$401F APU and I/O registers (TODO)
 
             // $4014 OAMDMA
-            memory.ConfigureMemoryRead(0x4014, (address) => Video.ObjectAttributeDirectMemoryAccess);
-            memory.ConfigureMemoryWrite(0x4014, (address, value) =>
+            Memory.ConfigureMemoryRead(0x4014, (address) => Video.ObjectAttributeDirectMemoryAccess);
+            Memory.ConfigureMemoryWrite(0x4014, (address, value) =>
             {
                 Video.ObjectAttributeDirectMemoryAccess = value;
 
@@ -97,8 +105,6 @@ namespace NesCore
             });
 
             // $4020-$FFFF Cartridge space: PRG ROM, PRG RAM, and mapper registers (TODO)
-
-            return memory;
         }
 
         private void ConnectController(ushort portAddress, Controller controller)

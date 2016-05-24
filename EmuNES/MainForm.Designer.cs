5346e58c0e1feb8040c306e841b1cdfed268dc7d
colinvella <colinvella@gmail.com>
2016-05-24 18:02:10 +0200

    Started GUI

--- EmuNES/MainForm.Designer.cs
+++ EmuNES/MainForm.Designer.cs
@@ -28,12 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.mainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.gameMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gameOpenMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gameExitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainMenuStrip
+            // 
+            this.mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameMenuItem});
+            this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mainMenuStrip.Name = "mainMenuStrip";
+            this.mainMenuStrip.Size = new System.Drawing.Size(496, 24);
+            this.mainMenuStrip.TabIndex = 0;
+            this.mainMenuStrip.Text = "Main Menu Strip";
+            // 
+            // gameMenuItem
+            // 
+            this.gameMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameOpenMenuItem,
+            this.gameExitMenuItem});
+            this.gameMenuItem.Name = "gameMenuItem";
+            this.gameMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.gameMenuItem.Text = "&Game";
+            // 
+            // gameOpenMenuItem
+            // 
+            this.gameOpenMenuItem.Name = "gameOpenMenuItem";
+            this.gameOpenMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.gameOpenMenuItem.Text = "&Open";
+            // 
+            // gameExitMenuItem
+            // 
+            this.gameExitMenuItem.Name = "gameExitMenuItem";
+            this.gameExitMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.gameExitMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.gameExitMenuItem.Text = "Exit";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(496, 441);
+            this.Controls.Add(this.mainMenuStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.mainMenuStrip;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EmuNES";
+            this.mainMenuStrip.ResumeLayout(false);
+            this.mainMenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip mainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem gameMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gameOpenMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gameExitMenuItem;
     }
 }
 

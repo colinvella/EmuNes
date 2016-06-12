77a534b4af53863eba74a7e4d79a2b65f7574c35
Colin <colinvella@gmail.com>
2016-06-12 23:20:04 +0200

    Some more work on input dialog.

--- EmuNES/Input/InputOptionsForm.Designer.cs
+++ EmuNES/Input/InputOptionsForm.Designer.cs
@@ -31,6 +31,10 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InputOptionsForm));
             this.okButton = new System.Windows.Forms.Button();
             this.cancelButton = new System.Windows.Forms.Button();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.controllerOneTabPage = new System.Windows.Forms.TabPage();
+            this.controllerTwoTabPage = new System.Windows.Forms.TabPage();
+            this.tabControl.SuspendLayout();
             this.SuspendLayout();
             // 
             // okButton
@@ -52,6 +56,35 @@
             this.cancelButton.Text = "&Cancel";
             this.cancelButton.UseVisualStyleBackColor = true;
             // 
+            // tabControl
+            // 
+            this.tabControl.Appearance = System.Windows.Forms.TabAppearance.FlatButtons;
+            this.tabControl.Controls.Add(this.controllerOneTabPage);
+            this.tabControl.Controls.Add(this.controllerTwoTabPage);
+            this.tabControl.Location = new System.Drawing.Point(12, 12);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(558, 350);
+            this.tabControl.TabIndex = 2;
+            // 
+            // controllerOneTabPage
+            // 
+            this.controllerOneTabPage.Location = new System.Drawing.Point(4, 28);
+            this.controllerOneTabPage.Name = "controllerOneTabPage";
+            this.controllerOneTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.controllerOneTabPage.Size = new System.Drawing.Size(550, 318);
+            this.controllerOneTabPage.TabIndex = 0;
+            this.controllerOneTabPage.Text = "Controller One";
+            // 
+            // controllerTwoTabPage
+            // 
+            this.controllerTwoTabPage.Location = new System.Drawing.Point(4, 28);
+            this.controllerTwoTabPage.Name = "controllerTwoTabPage";
+            this.controllerTwoTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.controllerTwoTabPage.Size = new System.Drawing.Size(550, 318);
+            this.controllerTwoTabPage.TabIndex = 1;
+            this.controllerTwoTabPage.Text = "Controller Two";
+            // 
             // InputOptionsForm
             // 
             this.AcceptButton = this.okButton;
@@ -59,6 +92,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.cancelButton;
             this.ClientSize = new System.Drawing.Size(582, 403);
+            this.Controls.Add(this.tabControl);
             this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.okButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -68,6 +102,7 @@
             this.Name = "InputOptionsForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Input Options";
+            this.tabControl.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -76,5 +111,8 @@
 
         private System.Windows.Forms.Button okButton;
         private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage controllerOneTabPage;
+        private System.Windows.Forms.TabPage controllerTwoTabPage;
     }
 }

151230ef87d863954efc4d0defdb81ce3119366d
colinvella <colinvella@gmail.com>
2016-06-14 15:59:39 +0200

    Created KeyboardState class to avoid passing dictionary around

---
+++ EmuNES/Input/KeyboardState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmuNES.Input
+{
+    class KeyboardState
+    {
+        public KeyboardState()
+        {
+            keyPressed = new Dictionary<Keys, bool>();
+            foreach (Keys key in Enum.GetValues(typeof(Keys)))
+                keyPressed[key] = false;
+        }
+
+        public bool this[Keys key]
+        {
+            get { return keyPressed[key]; }
+            set { keyPressed[key] = value; }
+        }
+
+        private Dictionary<Keys, bool> keyPressed;
+    }
+}

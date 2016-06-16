e454873a7cffe0ef936477ba7b801f82301cbb9b
colinvella <colinvella@gmail.com>
2016-06-16 09:33:38 +0200

    Disassembly form missing from last commit

---
+++ EmuNES/Debug/CodeDisassemblyForm.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace EmuNES.Debug
+{
+    partial class CodeDisassemblyForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(384, 261);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // CodeDisassemblyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.ControlBox = false;
+            this.Controls.Add(this.dataGridView);
+            this.Name = "CodeDisassemblyForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Code Disassembly";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.OnFormClosing);
+            this.Load += new System.EventHandler(this.OnFormLoad);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+    }
+}
---
+++ EmuNES/Debug/CodeDisassemblyForm.cs
@@ -0,0 +1,65 @@
+﻿using NesCore;
+using NesCore.Processor;
+using NesCore.Utility;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmuNES.Debug
+{
+    public partial class CodeDisassemblyForm : Form
+    {
+        public CodeDisassemblyForm()
+        {
+            InitializeComponent();            
+        }
+
+        public void Trace(NesCore.Console console)
+        {
+            ushort address = console.Processor.State.ProgramCounter;
+            byte opCode = console.Memory[address];
+
+            DisassemblyLine disassemblyLine = new DisassemblyLine();
+            disassemblyLine.Address = Hex.Format(address);
+            disassemblyLine.OpCode = opCode;
+
+            Instruction instruction = console.Processor.InstructionSet[opCode];
+            disassemblyLine.Instruction = instruction.Name;
+
+
+            if (!disassemblyLineMap.ContainsKey(address))
+            {
+                disassemblyLineMap[address] = disassemblyLine;
+                disassemblyLines.Add(disassemblyLine);
+                dataGridView.DataSource = new SortableBindingList<DisassemblyLine>(disassemblyLines);
+            }
+        }
+
+        private void OnFormLoad(object sender, EventArgs e)
+        {
+            disassemblyLineMap = new Dictionary<ushort, DisassemblyLine>();
+            disassemblyLines = new List<DisassemblyLine>();
+            disassemblyBindingList = new SortableBindingList<DisassemblyLine>(disassemblyLines);
+
+            dataGridView.AutoGenerateColumns = true;
+            dataGridView.DataSource = disassemblyBindingList;
+            dataGridView.Sort(dataGridView.Columns[0], ListSortDirection.Ascending);
+        }
+
+        private void OnFormClosing(object sender, FormClosingEventArgs formClosingEventArgs)
+        {
+            formClosingEventArgs.Cancel = true;
+        }
+
+        private Dictionary<ushort, DisassemblyLine> disassemblyLineMap;
+        private List<DisassemblyLine> disassemblyLines;
+        private SortableBindingList<DisassemblyLine> disassemblyBindingList;
+    }
+}

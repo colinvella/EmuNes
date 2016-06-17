455ac95ab3ae0633472596948c7795b81b9c4d89
Colin <colinvella@gmail.com>
2016-06-17 23:34:15 +0200

    Started adding input settings class

---
+++ EmuNES/Settings/InputSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmuNES.Settings
+{
+    [Serializable]
+    public class InputSettings
+    {
+        public InputSettings()
+        {
+            Joypads = new List<JoypadSettings>();
+        }
+
+        public List<JoypadSettings> Joypads { get; private set; }
+
+        public InputSettings Duplicate()
+        {
+            InputSettings copy = new InputSettings();
+            foreach (JoypadSettings joypad in Joypads)
+                copy.Joypads.Add(joypad.Duplicate());
+            return copy;
+        }
+    }
+}
---
+++ EmuNES/Settings/JoypadSettings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmuNES.Settings
+{
+    [Serializable]
+    public class JoypadSettings
+    {
+        public JoypadSettings() { }
+
+        public byte Id { get; set; }
+        public string Start { get; set; }
+        public string Select { get; set; }
+        public string A { get; set; }
+        public string B { get; set; }
+        public string Left { get; set; }
+        public string Right { get; set; }
+        public string Up { get; set; }
+        public string Down { get; set; }
+
+        public JoypadSettings Duplicate()
+        {
+            JoypadSettings copy = new JoypadSettings();
+            copy.Id = Id;
+            copy.Start = Start;
+            copy.Select = Select;
+            copy.A = A;
+            copy.B = B;
+            copy.Left = Left;
+            copy.Right = Right;
+            copy.Up = Up;
+            copy.Down = Down;
+            return copy;
+        }
+    }
+}

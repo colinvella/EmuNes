418ee35654aa873002e5e8474eaaf2f9aa2266cc
Colin <colinvella@gmail.com>
2016-06-18 22:32:24 +0200

    Added configuration mappings. Need to add storage

--- EmuNES/Settings/JoypadSettings.cs
+++ EmuNES/Settings/JoypadSettings.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using EmuNES.Input;
+using NesCore.Input;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace EmuNES.Settings
 {
@@ -35,6 +38,57 @@ namespace EmuNES.Settings
             copy.Up = Up;
             copy.Down = Down;
             return copy;
+        }
+
+        public string EncodeKeyboardMapping(Keys key)
+        {
+            return "key:" + key;
+        }
+
+        public string EncodeJoystickMapping(byte controllerId, EmuNES.Input.Button button)
+        {
+            return "joy:" + controllerId + ":" + button;
+        }
+
+        public ButtonPressed DecodeMapping(string mapping,
+            KeyboardState keyboardState,
+            GameControllerManager gameCopntrollerManager)
+        {
+            if (mapping.StartsWith("key:"))
+                return DencodeKeyboardMapping(mapping, keyboardState);
+            if (mapping.StartsWith("joy:"))
+                return DencodeJoystickMapping(mapping, gameCopntrollerManager);
+
+            return () => false;
+        }
+
+        public Joypad ConfigureJoypad(KeyboardState keyboardState, GameControllerManager gameControllerManager)
+        {
+            Joypad joypad = new Joypad();
+            joypad.Start = DecodeMapping(Start, keyboardState, gameControllerManager);
+            joypad.Select = DecodeMapping(Select, keyboardState, gameControllerManager);
+            joypad.A = DecodeMapping(A, keyboardState, gameControllerManager);
+            joypad.B = DecodeMapping(B, keyboardState, gameControllerManager);
+            joypad.Up = DecodeMapping(Up, keyboardState, gameControllerManager);
+            joypad.Down = DecodeMapping(Down, keyboardState, gameControllerManager);
+            joypad.Left = DecodeMapping(Left, keyboardState, gameControllerManager);
+            joypad.Right = DecodeMapping(Right, keyboardState, gameControllerManager);
+            return joypad;
+        }
+
+        private ButtonPressed DencodeKeyboardMapping(string mapping, KeyboardState keyboardState)
+        {
+            Keys key = (Keys)Enum.Parse(typeof(Keys), mapping.Replace("key:", ""));
+            return () => keyboardState[key];
+        }
+
+        private ButtonPressed DencodeJoystickMapping(string mapping, GameControllerManager gameControllerManager)
+        {
+            string[] tokens = mapping.Trim().Split(new char[] { ':' });
+            byte controllerId = byte.Parse(tokens[1]);
+            EmuNES.Input.Button button = (EmuNES.Input.Button)Enum.Parse(typeof(EmuNES.Input.Button), tokens[2]);
+            GameController gameController = gameControllerManager[controllerId];
+            return () => gameController[button];
         }
     }
 }

fea739d717efa5dd2f246afadddc3cd247f61684
Colin <colinvella@gmail.com>
2016-06-21 21:51:44 +0200

    Fixed extra comma in length table that somehow didn't trigger compiler error!

--- NesCore/Audio/Generators/WaveGenerator.cs
+++ NesCore/Audio/Generators/WaveGenerator.cs
@@ -29,7 +29,7 @@ namespace NesCore.Audio.Generators
 
         protected static readonly byte[] lengthTable = {
             10, 254, 20, 2, 40, 4, 80, 6, 160, 8, 60, 10, 14, 12, 26, 14,
-            12, 16, 24, 18, 48, 20, 96, 22, 192, 24, 72, 26, 16, 28, 32, 30,
+            12, 16, 24, 18, 48, 20, 96, 22, 192, 24, 72, 26, 16, 28, 32, 30
         };
 
         protected static readonly byte[][] dutyTable = {

f342a679dd9096ad8013825f21b84dd43c469272
Colin <colinvella@gmail.com>
2016-06-21 22:33:06 +0200

    extra comma in triangle table

--- NesCore/Audio/Generators/TriangleGenerator.cs
+++ NesCore/Audio/Generators/TriangleGenerator.cs
@@ -115,7 +115,7 @@ namespace NesCore.Audio.Generators
 
         private static readonly byte[] triangleTable = {
             15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0,
-            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
         };
     }
 }

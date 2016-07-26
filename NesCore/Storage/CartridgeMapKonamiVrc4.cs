ad31cee7bb01b2a4c0b5a209c7e7b4dc212ea885
colinvella <colinvella@gmail.com>
2016-07-26 12:24:06 +0200

    More work on VRC4

--- NesCore/Storage/CartridgeMapKonamiVrc4.cs
+++ NesCore/Storage/CartridgeMapKonamiVrc4.cs
@@ -18,64 +18,58 @@ namespace NesCore.Storage
         public CartridgeMapKonamiVrc4(Cartridge cartridge, Variant variant)
             : base(cartridge)
         {
-            registerOffsets = new byte[8];
-            programModeRegisterAddresses = new ushort[4];
-            programBank0RegisterAddresses = new ushort[8];
-            mirroringRegisterAddresses = new ushort[3];
+            characterBankRegisterLowAddresses = new ushort[8][];
 
-            registerOffsets[0] = 0x00;
-            registerOffsets[4] = 0x00;
+            characterBank = new int[8];
+
+            irqReloadLowAddress = 0xF000;
+
             switch (variant)
             {
                 case Variant.Vrc4RevAorC:
                     mapperName = "Konami VRC4a/VRC4c";
-                    // program regeister 0 addresses for VRC4a and VRC4c
-                    registerOffsets[1] = 0x02;
-                    registerOffsets[2] = 0x04;
-                    registerOffsets[3] = 0x06;
 
-                    registerOffsets[5] = 0x40;
-                    registerOffsets[6] = 0x80;
-                    registerOffsets[7] = 0xC0;
+                    programBank0RegisterAddresses = new ushort[] { 0x8000, 0x8002, 0x8004, 0x8006, 0x8040, 0x8080, 0x80C0 };
+                    programBank1RegisterAddresses = new ushort[] { 0xA000, 0xA002, 0xA004, 0xA006, 0xA040, 0xA080, 0xA0C0 };
+                    mirroringRegisterAddresses = new ushort[] { 0x9000, 0x9002, 0x9040 };
+                    programModeRegisterAddresses = new ushort[] { 0x9004, 0x9006, 0x90C0 };
+
+                    characterBankRegisterLowAddresses[0] = new ushort[] { 0xB000 };
+                    characterBankRegisterHighAddresses[0] = new ushort[] { 0xB002, 0xB040 };
+                    characterBankRegisterLowAddresses[1] = new ushort[] { 0xB004, 0x8080 };
+                    characterBankRegisterHighAddresses[1] = new ushort[] { 0xB006, 0xB0C0 };
+
+                    characterBankRegisterLowAddresses[2] = new ushort[] { 0xC000 };
+                    characterBankRegisterHighAddresses[2] = new ushort[] { 0xC002, 0xC040 };
+                    characterBankRegisterLowAddresses[3] = new ushort[] { 0xC004, 0xC080 };
+                    characterBankRegisterHighAddresses[3] = new ushort[] { 0xC006, 0xC0C0 };
+
+                    characterBankRegisterLowAddresses[4] = new ushort[] { 0xD000 };
+                    characterBankRegisterHighAddresses[4] = new ushort[] { 0xD002, 0xD040 };
+                    characterBankRegisterLowAddresses[5] = new ushort[] { 0xD004, 0xD080 };
+                    characterBankRegisterHighAddresses[5] = new ushort[] { 0xD006, 0xD0C0 };
+
+                    characterBankRegisterLowAddresses[6] = new ushort[] { 0xE000 };
+                    characterBankRegisterHighAddresses[6] = new ushort[] { 0xE002, 0xE040 };
+                    characterBankRegisterLowAddresses[7] = new ushort[] { 0xE004, 0xE080 };
+                    characterBankRegisterHighAddresses[7] = new ushort[] { 0xE006, 0xE0C0 };
+
+                    irqReloadHighAddresses = new ushort[] { 0xF002, 0xF040 };
+                    irqControlAddresses = new ushort[] { 0xF004, 0xF080 };
+                    irqAcknowledgeAddresses = new ushort[] { 0xF006, 0xF0C0 };
+
                     break;
                 case Variant.Vrc4RevBorD:
                     mapperName = "Konami VRC4b/VRC4d";
-                    // program regeister 0 addresses for VRC4b and VRC4d
-                    registerOffsets[1] = 0x02;
-                    registerOffsets[2] = 0x01;
-                    registerOffsets[3] = 0x03;
-
-                    registerOffsets[5] = 0x08;
-                    registerOffsets[6] = 0x04;
-                    registerOffsets[7] = 0x0C;
                     break;
                 case Variant.Vrc4RevEorF:
                     mapperName = "Konami VRC4e/VRC4f";
-                    // program regeister 0 addresses for VRC4b and VRC4d
-                    registerOffsets[1] = 0x04;
-                    registerOffsets[2] = 0x08;
-                    registerOffsets[3] = 0x0C;
-
-                    registerOffsets[5] = 0x01;
-                    registerOffsets[6] = 0x02;
-                    registerOffsets[7] = 0x03;
                     break;
             }
 
-            for (int index = 0; index < 8; index++)
-            {
-                programBank0RegisterAddresses[index] = (ushort)(0x8000 + registerOffsets[index]);
-            }
-            mirroringRegisterAddresses[0] = 0x9000;
-            mirroringRegisterAddresses[1] = (ushort)(0x9000 + registerOffsets[1]);
-            mirroringRegisterAddresses[1] = (ushort)(0x9000 + registerOffsets[5]);
-
-            programModeRegisterAddresses[0] = (ushort)(0x9000 + registerOffsets[2]);
-            programModeRegisterAddresses[1] = (ushort)(0x9000 + registerOffsets[3]);
-            programModeRegisterAddresses[2] = (ushort)(0x9000 + registerOffsets[6]);
-            programModeRegisterAddresses[3] = (ushort)(0x9000 + registerOffsets[7]);
-
             programBankCount = cartridge.ProgramRom.Count / 0x2000;
+            characterBankCount = cartridge.CharacterRom.Length / 0x400;
+
         }
 
         public override string Name { get { return mapperName; } }
@@ -84,7 +78,14 @@ namespace NesCore.Storage
         {
             get
             {
-                throw new NotImplementedException();
+                if (address < 0x2000)
+                {
+                    int bankIndex = address / 0x400;
+                    int bankOffset = address % 0x400;
+                    return Cartridge.CharacterRom[characterBank[bankIndex] + bankOffset];
+                }
+                else
+                    return (byte)(address >> 8); // open buss
             }
 
             set
@@ -95,7 +96,11 @@ namespace NesCore.Storage
                 }
                 else if (programBank0RegisterAddresses.Contains(address))
                 {
-                    programBankIndex0 = value % programBankCount;
+                    programBank0 = value % programBankCount;
+                }
+                else if (programBank1RegisterAddresses.Contains(address))
+                {
+                    programBank1 = value % programBankCount;
                 }
                 else if (mirroringRegisterAddresses.Contains(address))
                 {
@@ -107,20 +112,70 @@ namespace NesCore.Storage
                         case 3: MirrorMode = MirrorMode.Single1; break;
                     }
                 }
+                else if (address >= 0xB000 && address < 0xF000)
+                {
+                    for (int characterBankIndex = 0; characterBankIndex < 8; characterBankIndex++)
+                    {
+                        if (characterBankRegisterLowAddresses[characterBankIndex].Contains(address))
+                        {
+                            characterBank[characterBankIndex] &= 0x1F0;
+                            characterBank[characterBankIndex] |= (value & 0x0F);
+                            break;
+                        }
+                        else if (characterBankRegisterHighAddresses[characterBankIndex].Contains(address))
+                        {
+                            characterBank[characterBankIndex] &= 0x00F;
+                            characterBank[characterBankIndex] |= ((value & 0x1F) << 4);
+                            break;
+                        }
+                    }
+                }
+                else if (address == irqReloadLowAddress)
+                {
+                    irqReloadValue &= 0xF0;
+                    irqReloadValue |= (byte)(value & 0x0F);
+                }
+                else if (irqReloadHighAddresses.Contains(address))
+                {
+                    irqReloadValue &= 0x0F;
+                    irqReloadValue |= (byte)((value & 0x0F) << 4);
+                }
+                else if (irqControlAddresses.Contains(address))
+                {
+
+                }
+                else if (irqAcknowledgeAddresses.Contains(address))
+                {
+
+                }
             }
         }
 
         private Variant variant;
         private string mapperName;
 
-        private byte[] registerOffsets;
         private ushort[] programModeRegisterAddresses;
         private ushort[] programBank0RegisterAddresses;
+        private ushort[] programBank1RegisterAddresses;
         private ushort[] mirroringRegisterAddresses;
+
+        private ushort[][] characterBankRegisterLowAddresses;
+        private ushort[][] characterBankRegisterHighAddresses;
+
+        private ushort irqReloadLowAddress;
+        private ushort[] irqReloadHighAddresses;
+        private ushort[] irqControlAddresses;
+        private ushort[] irqAcknowledgeAddresses;
 
         private ProgramMode programMode;
         private int programBankCount;
-        private int programBankIndex0;
+        private int programBank0;
+        private int programBank1;
+
+        private int characterBankCount;
+        private int[] characterBank;
+
+        private byte irqReloadValue;
 
         private enum ProgramMode
         {

2b7059bf55734422920556209b55203fc8dc890f
colinvella <colinvella@gmail.com>
2016-08-16 18:07:07 +0200

    Added zapper image

--- EmuNES/Properties/Resources.Designer.cs
+++ EmuNES/Properties/Resources.Designer.cs
@@ -300,5 +300,15 @@ namespace SharpNes.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Zapper {
+            get {
+                object obj = ResourceManager.GetObject("Zapper", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }

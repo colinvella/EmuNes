e1f0ddfbe037131cb5e0db892bbb7fe65387f13b
colinvella <colinvella@gmail.com>
2016-08-22 15:01:21 +0200

    Mapper 41 - added implementation - prg seems ok but chr needs work

--- NesCore/Storage/CartridgeMapCaltron6in1.cs
+++ NesCore/Storage/CartridgeMapCaltron6in1.cs
@@ -16,13 +16,31 @@ namespace NesCore.Storage
 
         public override string Name { get { return "Caltron 6-in-1"; } }
 
-
         public override byte this[ushort address]
         {
             get
             {
-                Debug.WriteLine(Name + ": Unexpected read from address " + Hex.Format(address));
-                return (byte)(address >> 8); // return open bus
+                if (address < 0x2000)
+                {
+                    int flatAddress = characterBankOuter * 0x8000;
+                    int bankOffset = address % 0x8000;
+                    if (characterBankInnerEnabled)
+                    {
+                        flatAddress += characterBankInner * 0x2000;
+                        bankOffset = address % 0x2000;
+                    }
+                    flatAddress += bankOffset;
+                    return Cartridge.CharacterRom[flatAddress];
+                }
+                else if (address >= 0x8000)
+                {
+                    return Cartridge.ProgramRom[programBank * 0x8000 + address % 0x8000];
+                }
+                else
+                {
+                    Debug.WriteLine(Name + ": Unexpected read from address " + Hex.Format(address));
+                    return (byte)(address >> 8); // return open bus
+                }
             }
 
             set
@@ -48,6 +66,13 @@ namespace NesCore.Storage
                     Debug.WriteLine(Name + ": Unexpected write of value " + Hex.Format(value) + " at address " + Hex.Format(address));
                 }
             }
+        }
+
+        public override void Reset()
+        {
+            programBank = characterBankOuter = characterBankInner = 0;
+            characterBankInnerEnabled = false;
+            MirrorMode = MirrorMode.Vertical;
         }
 
         private int programBank;
